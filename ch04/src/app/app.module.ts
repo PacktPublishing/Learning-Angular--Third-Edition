76bebf3a6dc81d67498b490b72acf865d503ad8c
Aristeidis Bampakos <bampakoa@gmail.com>
2020-02-29 17:55:17 +0200

    Add ngIf example

---
+++ ch04/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HeroComponent } from './hero/hero.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ ch04/src/app/hero/hero.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-hero',
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.css']
+})
+export class HeroComponent implements OnInit {
+
+  @Input() name: string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
