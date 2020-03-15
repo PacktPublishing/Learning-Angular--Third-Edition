6c3431e806151ff1c7ca9af4399c0c344ea95480
Aristeidis Bampakos <bampakoa@gmail.com>
2020-03-15 19:10:49 +0200

    Create public hero component

--- ch05/src/app/app.module.ts
+++ ch05/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeroesModule } from './heroes/heroes.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ch05/src/app/heroes/heroes.module.ts
+++ ch05/src/app/heroes/heroes.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { HeroComponent } from './hero/hero.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [HeroComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [HeroComponent]
 })
 export class HeroesModule { }
