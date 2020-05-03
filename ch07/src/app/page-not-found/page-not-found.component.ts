2bb1d099b59f77abf1794aa8bca0881c2cbabcec
Aristeidis Bampakos <bampakoa@gmail.com>
2020-05-03 19:45:43 +0300

    Navigate to a route imperatively. Add styling to active routes.

--- ch07/src/app/page-not-found/page-not-found.component.ts
+++ ch07/src/app/page-not-found/page-not-found.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html',
   styleUrls: ['./page-not-found.component.css']
 })
-export class PageNotFoundComponent implements OnInit {
+export class PageNotFoundComponent {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit(): void {
+  goHome() {
+    this.router.navigate(['/']);
   }
 
 }
