8c3f90bcda600f492143af32ca6796abfeff025d
Aristeidis Bampakos <bampakoa@gmail.com>
2020-05-13 23:14:37 +0300

    Create simple login form

--- ch08/src/app/app.module.ts
+++ ch08/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
