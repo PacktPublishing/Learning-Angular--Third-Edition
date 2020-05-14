4db333ab5fac59c6dbd5ae7a7410188439260f28
Aristeidis Bampakos <bampakoa@gmail.com>
2020-05-14 23:08:55 +0300

    Upgrade to Angular 10.0.0-next.7

--- ch06/README.md
+++ ch06/README.md
@@ -1,6 +1,6 @@
 # MyApp
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.0.0-next.4.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.0.0-next.5.
 
 ## Development server
 
--- ch08/src/app/login/login.component.ts
+++ ch08/src/app/login/login.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  username: string;
+  password: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  login() {
+    console.log('User: ' + this.username);
+    console.log('Password: ' + this.password);
   }
-
 }
