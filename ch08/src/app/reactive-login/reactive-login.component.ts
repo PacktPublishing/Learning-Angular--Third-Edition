26d65dcc647f6692bca733f6ecbc1de639b9f40f
Aristeidis Bampakos <bampakoa@gmail.com>
2020-05-17 23:55:08 +0300

    Add validation example

--- ch08/src/app/reactive-login/reactive-login.component.ts
+++ ch08/src/app/reactive-login/reactive-login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, AbstractControl } from '@angular/forms';
+import { FormGroup, FormControl, AbstractControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-login',
@@ -9,12 +9,19 @@ import { FormGroup, FormControl, AbstractControl } from '@angular/forms';
 export class ReactiveLoginComponent implements OnInit {
 
   loginForm = new FormGroup({
-    username: new FormControl(''),
-    password: new FormControl('')
+    username: new FormControl('', Validators.required),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6)
+    ])
   });
 
   get username(): AbstractControl {
     return this.loginForm.controls.username;
+  }
+
+  get password(): AbstractControl {
+    return this.loginForm.controls.password;
   }
 
   constructor() { }
@@ -24,7 +31,6 @@ export class ReactiveLoginComponent implements OnInit {
 
   login() {
     console.log('User: ' + this.username.value);
-    console.log('Password: ' + this.loginForm.controls.password.value);
+    console.log('Password: ' + this.password.value);
   }
 }
-
